d70a8e53a8e9950f0c9125367ebd6a2c6e98f715
Md Imran Alam <mdimranalam2348@gmail.com>
2024-03-15 23:36:55 +0530

    Update Var.js

--- 01_Basic/Var.js
+++ 01_Basic/Var.js
@@ -1,7 +1,17 @@
+    
+/*javaScript has 8 Datatypes
+1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined
+6. Null
+7. Symbol
+8. Object*/
+
+ str= "furkan"
 StudentName="md imran alam";
-
 age=23;
-
 phoneNumber=9504563258;
 
 console.log(StudentName);
