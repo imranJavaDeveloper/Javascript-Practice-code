9eb7fd9b793d2881a7aa7f40114bdeba0a65c8c7
Md Imran Alam <mdimranalam2348@gmail.com>
2024-04-17 13:18:43 +0530

    Update Airthmetic.js

--- 02_Operator/Airthmetic.js
+++ 02_Operator/Airthmetic.js
@@ -2,6 +2,6 @@
 let a=45;
 let b=25;
 console.log("the value of a =" ,a+" & value of b ="+b);
-console.log("a -b "+a-b);
-console.log("a*b "+a*b);
-console.log("a/b "+a/b);
+console.log("a -b ="+a-b);
+console.log("a*b ="+a*b);
+console.log("a/b ="+a/b);
