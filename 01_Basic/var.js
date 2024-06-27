7147b0c13539cfcb899c949d61fc40f2bb7c2bd3
Md Imran Alam <mdimranalam2348@gmail.com>
2024-06-27 21:00:04 +0530

    Variable Example in Javascript

---
+++ 01_Basic/var.js
@@ -0,0 +1,19 @@
+/**
+ * String
+ * boolean
+ * undefined
+ * Null
+ * Bigint
+ */
+
+// Example
+   fullName="md furkan alam";
+   age =22;
+   address="Nawada Bihar"
+   pincode=805110;
+
+   // show all deatil
+   console.log(fullName);
+   console.log(age);
+   console.log(address);
+   console.log(pincode);
