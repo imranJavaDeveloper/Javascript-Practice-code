135ef45a33724b0b391c34d4e7f440c87d2fb564
Md Imran Alam <mdimranalam2348@gmail.com>
2024-06-27 22:59:49 +0530

    Update var.js

--- 01_Basic/var.js
+++ 01_Basic/var.js
@@ -7,13 +7,40 @@
  */
 
 // Example
-   fullName="md furkan alam";
-   age =22;
-   address="Nawada Bihar"
-   pincode=805110;
+  let fullName="md furkan alam";
+  let age =22;
+  let address="Nawada Bihar"
+  const pincode=805110;
+
+  // let keyword 
+
+  {
+    let a=55;
+    console.log(a);
+  }
+
+  console.log(a);
+
+   
+   x=null;
+   y=undefined;
 
    // show all deatil
    console.log(fullName);
    console.log(age);
    console.log(address);
    console.log(pincode);
+
+   /* variable Rule
+
+   1. variable name is case sensitive
+   2. only latter ,digit,underscore(_) and $ symbol is allow.(not even space)
+   3. only latter and underscore(_) or $ should be first character
+   4. keyword not allow */
+
+
+
+   
+
+
+   
