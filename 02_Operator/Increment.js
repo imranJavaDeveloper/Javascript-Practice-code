ad97f5c37bc09edf03e188604f8d8e76eb2c595c
Md Imran Alam <mdimranalam2348@gmail.com>
2024-07-13 21:37:47 +0530

    Update Increment.js

--- 02_Operator/Increment.js
+++ 02_Operator/Increment.js
@@ -11,3 +11,15 @@ x++;
 // Place the operator before the variable (prefix)
 ++x;
 console.log(x);
+
+
+
+// Another example
+let z = 5;
+let y = 5;
+
+console.log(x++); // 5
+console.log(x); // 6
+
+console.log(++y); // 6
+console.log(y); // 6
