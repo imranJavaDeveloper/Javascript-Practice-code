7f34c7c608ece871dbaa8340bcc35269fa653c6c
Md Imran Alam <mdimranalam2348@gmail.com>
2024-07-13 20:53:52 +0530

    increment

---
+++ 02_Operator/Increment.js
@@ -0,0 +1,13 @@
+let x = 5;
+
+x++;
+
+console.log(x); // 6
+
+
+// Place the operator next to the variable (postfix)
+x++;
+
+// Place the operator before the variable (prefix)
+++x;
+console.log(x);
