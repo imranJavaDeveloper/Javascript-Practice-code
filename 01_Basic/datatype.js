af5025693f53c0efa81e7b0c10cda1d87b0ac39f
Md Imran Alam <mdimranalam2348@gmail.com>
2024-07-14 23:43:13 +0530

    datatype example

---
+++ 01_Basic/datatype.js
@@ -0,0 +1,20 @@
+/* Practice on data type  */
+
+console.log("example of data type");
+age=45;
+firstName="md";
+middileName="furkan";
+lastName="Alam";
+
+// print all value
+console.log(age);
+console.log(firstName)
+console.log(middileName)
+console.log(lastName);
+
+
+// create variable using the let keyword
+
+   let age=45;
+   let fullName="Md Furkan Alam";
+   let phoneNumber=9109495918;
