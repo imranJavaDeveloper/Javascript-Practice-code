74f100b58ad9fd264a29203e81076209fac7b150
Md Imran Alam <mdimranalam2348@gmail.com>
2024-07-14 23:57:53 +0530

    Update datatype.js

--- 01_Basic/datatype.js
+++ 01_Basic/datatype.js
@@ -15,7 +15,7 @@ console.log(lastName);
 
 // create variable using the let keyword
 
-   let age=45;
+   let age2=45;
    let fullName="Md Furkan Alam";
    let phoneNumber=9109495918;
 
@@ -34,6 +34,18 @@ console.log(name);
   let y;
 console.log(x); // undefined
 console.log(y);
+
+// in const keyword we  can not use undefined 
+ 
+ //  const a;
+console.log(a); // error variable a not inilition
+
+let s="md Atif alam";
+
+console.log(s);
+console.log(typeof s); // type of used for check data type of the variable
+
+ 
 
 
       
