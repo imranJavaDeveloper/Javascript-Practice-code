374388cbdf8402b90d0fb42494c9ea8d1fb866fe
imranJavaDeveloper <mdimranalam2348@gmail.com>
2025-02-06 21:45:24 +0530

    Airthmetic Operator Example

---
+++ 02_Operator/Airthmetic/Airtmetic2.js
@@ -0,0 +1,24 @@
+
+
+let first=45;
+let second=5;
+
+console.log(first+second);//50
+
+console.log(first-second);// 40
+
+console.log(first*second);// 225
+
+console.log(first/second);// 9
+
+console.log(first%second);//0
+
+
+
+
+/** Example second */
+console.log("EXAMPLE SECOND")
+let a=455;
+let b=48;
+
+console.log(a/b);
