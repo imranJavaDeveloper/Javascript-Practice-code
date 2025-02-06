2a28911ebe5b1d9335736e348aae71cfadfb1c45
imranJavaDeveloper <mdimranalam2348@gmail.com>
2025-02-07 02:01:18 +0530

    Update Example.js

--- 02_Operator/comparison operators/Example.js
+++ 02_Operator/comparison operators/Example.js
@@ -19,13 +19,58 @@ console.log("===================")
     console.log(first != second); // true
 
     let thir=55;
-    console.log( thir !== second);// false
+    console.log( thir !== second);// true 
+}
+
+
+{
+
+    console.log("===============");
+
+     let a=45;
+     let b=45;
+     console.log(  a != b) // false
+
+     let c="45";
+     console.log(a != c); // false
+
+     console.log(a !== c);// true
+}
+
+{
+
+    console.log("=========================")
+
+   /**  let a=6;
+    let b=true;
+
+    console.log(a != b); //  true*/
+
+/** 
+    let a=20;
+    let b=50;
+    console.log(a!=b); // true */
+
+
+    let a=20
+    let b=50;
+    console.log(a!==b); // true 
+
+
+    let a2=20
+    let b2="20";
+    console.log(a!==b); // true 
+
+
+    console.log("===========");
+
+    let f1=20;
+    let f2=50;
+    console.log(f1 !=f2);// true
+
+    let f3 ="20";
+    console.log(f1 != f3);// false
     
-
-    
-
-
-
-
+   
 }
 
