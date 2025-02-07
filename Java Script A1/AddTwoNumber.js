0d85a186e87872caa496ad709a2d8c4f18ec9883
imranJavaDeveloper <mdimranalam2348@gmail.com>
2025-02-07 17:25:34 +0530

    Basic Question solve in java script

---
+++ Java Script A1/AddTwoNumber.js
@@ -0,0 +1,11 @@
+
+/** create a function add two number  */
+
+function addTwoNumber(firstNumber , secondNumber){
+
+    let result=firstNumber + secondNumber;
+    console.log(result);
+}
+/***function call */
+
+addTwoNumber(2,5);
