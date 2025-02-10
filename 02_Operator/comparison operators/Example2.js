ac4a8f68be3b82b2648251342942b3094b0fa8cf
imranJavaDeveloper <mdimranalam2348@gmail.com>
2025-02-11 02:24:29 +0530

    == and === equal operaot example

---
+++ 02_Operator/comparison operators/Example2.js
@@ -0,0 +1,27 @@
+
+{
+    let a=10;
+    let b=20;
+    console.log(a==b);// false
+    console.log(a!=b);// true
+    
+}
+
+console.log("=======================");
+
+{
+    let num1=20;
+    let num2="2";
+    
+    /**console.log(num1==num2);
+    console.log(num1===num2);*/
+
+    console.log(num1!==num2);// true
+    console.log(num1!=num2); //true
+
+
+    
+    
+    
+    
+}
