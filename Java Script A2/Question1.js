3a5686717f56d09475dcc9dbdb7690b1fa5be908
imranJavaDeveloper <mdimranalam2348@gmail.com>
2025-02-12 02:00:25 +0530

    Create Question1.js

---
+++ Java Script A2/Question1.js
@@ -0,0 +1,29 @@
+
+
+let m=5;
+switch(m){
+
+    case m<=10:
+        console.log("E");
+        break;
+
+     case m>=11 && m<=20:
+        console.log("D");
+        break; 
+        
+        case m>= 21 && m<=30:
+        console.log("C");
+        break; 
+
+        case m>= 31 && m<=40:
+        console.log("B");
+        break;
+        
+        case m>= 41 && m<=50:
+        console.log("A");
+        break;
+        
+        default:
+            console.log("worong input");
+            
+}
