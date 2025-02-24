ca45bfe41c4a370b2d30bccc9c16265f351ff9c6
imranJavaDeveloper <mdimranalam2348@gmail.com>
2025-02-24 09:37:21 +0530

    Function Example In java Scaript

---
+++ 07_Function/Simple.js
@@ -0,0 +1,24 @@
+
+
+/** creating A  simple function in java Script */
+
+function fun(){
+    console.log("hello world");
+    
+}
+fun();// function call
+
+
+/** create a function take two number value print there sum */
+
+function add(a,b){
+
+    let c=a+b;
+    console.log("sum of two number is :"+c);
+
+    
+}
+
+/** function call */
+
+add(10,20); // sum of two number is :30
