12066aceeec94363332dc20c1c0473a7998164f7
imranJavaDeveloper <mdimranalam2348@gmail.com>
2025-02-25 13:58:52 +0530

    Find Max Value in the Array in js

---
+++ 05_Array/VolwelAndConst.js
@@ -0,0 +1,15 @@
+
+
+// find volwel and consonant
+
+let  char=prompt("enter a character");
+
+   if(char==='A','E','I','O','U' || 'a','i','e','o','u'){
+    console.log("volwel");
+    
+   }
+   else{
+    console.log("consonant");
+    
+   }
+
