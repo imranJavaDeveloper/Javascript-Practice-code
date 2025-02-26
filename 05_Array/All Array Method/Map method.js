8dd978341a12359f159ddad7235b6b9081587b62
imranJavaDeveloper <mdimranalam2348@gmail.com>
2025-02-26 13:17:36 +0530

    map method example in js

---
+++ 05_Array/All Array Method/Map method.js
@@ -0,0 +1,30 @@
+
+/**  example of map method in js */
+
+let A=[4,5,3,8];
+
+/** map method apply on Array  */
+ let result=A.map(x => x*2);
+ console.log(result); //  [8, 10, 6, 16]
+ 
+
+ /**  example 2 */
+
+let A2=[4,5,3,8];
+
+/** map method apply on Array  */
+ let result2=A.map(x => (x-2)); 
+ console.log(result2); // [2, 3, 1, 6]
+ 
+ 
+
+ /*** Example 3 */
+   
+ let A3=["Mohan","Gita","Rakesh"];
+
+ /** map() method  */
+
+ let res=A3.map(f => "hello " +f);
+ console.log(res);
+ 
+ 
