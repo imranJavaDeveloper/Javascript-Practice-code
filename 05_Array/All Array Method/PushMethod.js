463bb8e3e7bd931d88379c39e9072e035a7a5561
imranJavaDeveloper <mdimranalam2348@gmail.com>
2025-02-26 12:07:30 +0530

    ArrayExamplein js

---
+++ 05_Array/All Array Method/PushMethod.js
@@ -0,0 +1,11 @@
+
+
+/** Add element in the end */
+
+const arr=[4,87,54,214,5,5];
+arr.push=21;
+
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+    
+}
---
+++ 05_Array/Basic/SimpleExample.js
@@ -0,0 +1,37 @@
+
+
+
+let marksArray=[45,24,85,65,45,78];
+
+console.log(marksArray[0]);
+console.log(marksArray[1]);
+console.log(marksArray[2]);
+console.log(marksArray[3]);
+console.log(marksArray[4]);
+console.log(marksArray[5]);
+
+console.log("============================");
+
+let arr= new Array(11,2,4,5,8,7,5,);
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+
+
+/*** insert element on particular index */
+
+const myArray=["apple","orange","Guava"];
+  myArray[1]="pineApple";
+
+  for(let i=0;i<myArray.length;i++){
+       console.log(myArray[i]);
+       
+  }
+
+
+
+
