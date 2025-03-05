74e3c0c13d574db34246d4a150ea6a5cd73cf190
imranJavaDeveloper <mdimranalam2348@gmail.com>
2025-03-05 14:55:35 +0530

    Regular Function Example in javaScript

---
+++ 07_Function/RegularFunction.js
@@ -0,0 +1,33 @@
+
+
+
+/** create a function provide operation like
+ *  1 addition
+ *  2 subtraction
+ *  3 multiplication
+ * 
+ */
+
+ /**Addition Program */
+
+ function add( a,b){
+    let sum=a+b;
+    console.log(sum); 
+ }
+ add(45,25);
+
+ /*** 2 subtraction  */
+ function sub( a,b){
+    let sum=a-b;
+    console.log(sum); 
+ }
+ sub(45,25);
+
+  /*** 3 multiplication  */
+  function mul(a,b){
+    let mul=a*b;
+    console.log(mul);
+    
+  }
+mul(5,20);
+
