e6fcb3dcf146989821ddc8816f3c1c3039be2c72
imranJavaDeveloper <mdimranalam2348@gmail.com>
2025-03-15 16:04:45 +0530

    Create example3.js

---
+++ 02_Operator/comparison operators/example3.js
@@ -0,0 +1,31 @@
+
+
+/*** comparasion operator  */
+
+let a=45;
+let b="45";
+
+// console.log(a==b); // false
+
+//console.log(a!=b); // ture
+
+
+// console.log(a===b);// false
+
+// console.log(a!==b);// true
+
+let c=20;
+let d="20";
+console.log(c!=d);// false
+console.log(c!==d); // ture
+
+
+
+ 
+
+
+
+
+
+
+
