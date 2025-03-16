3c5faebce940ae46f7ae7dfc5c1e11dcf43af7f5
imranJavaDeveloper <mdimranalam2348@gmail.com>
2025-03-16 20:04:11 +0530

    datatype practice

---
+++ 01_Basic/datatypes.js
@@ -0,0 +1,45 @@
+
+
+
+/*** All primitive data type in js */
+// String
+ // Number
+ // BigInt
+ // Boolean
+ // Symbol
+ // Null
+ // Undefined
+
+
+ let str="md imran alam";
+ console.log(str);
+ 
+ Number =5558544;
+ console.log(Number);
+ BigInt=5478556689874566544n;
+ console.log(BigInt);
+
+ Symbol=4785554556;
+ console.log(Symbol);
+
+ Symbol = "MD IMRAN ALAM"
+ console.log(Symbol);
+ 
+ /***** Example of hoisting in js */
+
+ x=10;
+ console.log(x);
+ let x; /// can not access x before inilization
+ 
+
+ 
+ console.log(x2);
+ x2=10;
+ var x2; /// 10
+ 
+ 
+ 
+ 
+ 
+ 
+ 
