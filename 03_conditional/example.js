fd3adbc5d14da33f2b8b33929a04e038bec41528
imranJavaDeveloper <mdimranalam2348@gmail.com>
2025-03-16 09:10:44 +0530

    if else example

---
+++ 03_conditional/example.js
@@ -0,0 +1,17 @@
+
+
+let age=28;
+
+if(age>18){
+    console.log("you can vote");
+    console.log("you can drive");
+    console.log("you can drink");
+
+    
+} else{
+    console.log("you can not drive");
+    console.log("you can not vote");
+    console.log("you can not drink");
+    
+    
+}
---
+++ 04_loops/for loop/example1.js
@@ -0,0 +1,8 @@
+
+
+/*** example of for loop */
+
+for(let i=0;i<=10;i++){
+    console.log("*");
+    
+}
