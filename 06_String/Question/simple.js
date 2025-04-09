2da047b381c7c5e7f73fa4db9f5bc02f7ebbf91b
imranJavaDeveloper <mdimranalam2348@gmail.com>
2025-04-09 16:02:12 +0530

    practice indexOf() String method

---
+++ 06_String/Question/simple.js
@@ -0,0 +1,18 @@
+
+console.log("hello JavaScript Develoer");
+
+/**** method first ****/
+
+// indexOf();
+
+let str= 'hello developers ';
+let result=str.indexOf('e');
+console.log(result);
+
+let result2=str.lastIndexOf('e');
+console.log(result2);
+
+// find the index of 'e' after 8th index
+let result3=str.indexOf('e',8);
+console.log(result3);
+
