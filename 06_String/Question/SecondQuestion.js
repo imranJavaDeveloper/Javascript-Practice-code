1b3741f928f59b710b22a61be88c05e0d09c572a
imranJavaDeveloper <mdimranalam2348@gmail.com>
2025-04-09 16:30:47 +0530

    String Concatenation method example

---
+++ 06_String/Question/SecondQuestion.js
@@ -0,0 +1,18 @@
+
+/*** Add two or more String in single String ***/
+
+let firstString="Hi ";
+
+let secondString=" JavaScript ";
+let thirdString=" Developer ";
+
+let result=firstString+secondString+thirdString;
+console.log(result);
+
+
+
+let Str="hello everyone ";
+let str2=" my favourite Language is JavaScript ";
+let str3=" and i am learning it now from cetpa Infotech ";
+let result2=Str+str2+str3;
+console.log(result2);
